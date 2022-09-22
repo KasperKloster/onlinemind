4f34c3c21f3c52cb235b2cb9ead25083dca515d7
Kasper Kloster <kasperkloster@Kaspers-MacBook-Pro-2021.local>
2022-09-22 12:10:52 +0800

    Updated to Bootstrap 5

--- app/Http/Policies/Extended.php
+++ app/Http/Policies/Extended.php
@@ -12,45 +12,46 @@ class Extended extends Basic
 {
     public function configure()
     {
-        parent::configure();
+      parent::configure();
 
-		// Allow scripts from Bootstrap
-        $this->addDirective(Directive::DEFAULT, 'https://maxcdn.bootstrapcdn.com');
-        $this->addDirective(Directive::SCRIPT, 'https://maxcdn.bootstrapcdn.com');
-        $this->addDirective(Directive::STYLE, 'https://maxcdn.bootstrapcdn.com');
-        $this->addDirective(Directive::FONT, 'https://maxcdn.bootstrapcdn.com');
-
-        // Allow scripts from Google.com // Fonts, Tag Manager, Analytics
-        $this->addDirective(Directive::DEFAULT, 'https://fonts.gstatic.com');
-        $this->addDirective(Directive::STYLE, 'https://fonts.googleapis.com');
-        $this->addDirective(Directive::SCRIPT, 'https://fonts.googleapis.com');
-        $this->addDirective(Directive::FONT, 'https://fonts.gstatic.com');
+		  // Allow scripts from Bootstrap
+      $this->addDirective(Directive::DEFAULT, 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/');
+      $this->addDirective(Directive::SCRIPT, 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js');
+      $this->addDirective(Directive::STYLE, 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css');
+      $this->addDirective(Directive::FONT, 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/');
         
-        $this->addDirective(Directive::SCRIPT, '*.googletagmanager.com');
-        $this->addDirective(Directive::STYLE, '*.googletagmanager.com');
-        $this->addDirective(Directive::IMG, '*.googletagmanager.com');
 
-        $this->addDirective(Directive::DEFAULT, '*.google-analytics.com');
-        $this->addDirective(Directive::SCRIPT, '*.google-analytics.com');
-        $this->addDirective(Directive::STYLE, '*.google-analytics.com');
-        $this->addDirective(Directive::IMG, '*.google-analytics.com');
-        $this->addDirective(Directive::CONNECT, '*.google-analytics.com');
+      // Allow scripts from Google.com // Fonts, Tag Manager, Analytics
+      $this->addDirective(Directive::DEFAULT, 'https://fonts.gstatic.com');
+      $this->addDirective(Directive::STYLE, 'https://fonts.googleapis.com');
+      $this->addDirective(Directive::SCRIPT, 'https://fonts.googleapis.com');
+      $this->addDirective(Directive::FONT, 'https://fonts.gstatic.com');
+      
+      $this->addDirective(Directive::SCRIPT, '*.googletagmanager.com');
+      $this->addDirective(Directive::STYLE, '*.googletagmanager.com');
+      $this->addDirective(Directive::IMG, '*.googletagmanager.com');
 
-        $this->addDirective(Directive::DEFAULT, '*.google-analytics.com/j/collect');
-        $this->addDirective(Directive::SCRIPT, '*.google-analytics.com/j/collect');
-        $this->addDirective(Directive::STYLE, '*.google-analytics.com/j/collect');
+      $this->addDirective(Directive::DEFAULT, '*.google-analytics.com');
+      $this->addDirective(Directive::SCRIPT, '*.google-analytics.com');
+      $this->addDirective(Directive::STYLE, '*.google-analytics.com');
+      $this->addDirective(Directive::IMG, '*.google-analytics.com');
+      $this->addDirective(Directive::CONNECT, '*.google-analytics.com');
+
+      $this->addDirective(Directive::DEFAULT, '*.google-analytics.com/j/collect');
+      $this->addDirective(Directive::SCRIPT, '*.google-analytics.com/j/collect');
+      $this->addDirective(Directive::STYLE, '*.google-analytics.com/j/collect');
         
-        // Allow JavaScript (jQuery and Bootstrap)
-        $this->addDirective(Directive::SCRIPT, 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js');
-        $this->addDirective(Directive::SCRIPT, 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js');
-        $this->addDirective(Directive::SCRIPT, 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js');
+      // Allow JavaScript (jQuery and Bootstrap)
+      $this->addDirective(Directive::SCRIPT, 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js');
+      $this->addDirective(Directive::SCRIPT, 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js');
+      // $this->addDirective(Directive::SCRIPT, 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js');
 
-        // fontawesome
-        $this->addDirective(Directive::DEFAULT, 'https://use.fontawesome.com/');
-        $this->addDirective(Directive::SCRIPT, 'https://use.fontawesome.com/');
-        $this->addDirective(Directive::STYLE, 'https://use.fontawesome.com/');
-        $this->addDirective(Directive::FONT, 'https://use.fontawesome.com/');
-        $this->addDirective(Directive::IMG, 'https://use.fontawesome.com/');
+      // fontawesome
+      $this->addDirective(Directive::DEFAULT, 'https://use.fontawesome.com/');
+      $this->addDirective(Directive::SCRIPT, 'https://use.fontawesome.com/');
+      $this->addDirective(Directive::STYLE, 'https://use.fontawesome.com/');
+      $this->addDirective(Directive::FONT, 'https://use.fontawesome.com/');
+      $this->addDirective(Directive::IMG, 'https://use.fontawesome.com/');
 
     }
 }
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -87,8 +87,7 @@
       <!-- jQuery first, then Popper.js, then Bootstrap JS -->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
       <script defer src="https://use.fontawesome.com/releases/v5.1.1/js/all.js" integrity="sha384-BtvRZcyfv4r0x/phJt9Y9HhnN5ur1Z+kZbKVgzVBAlQZX4jvAuImlIz+bG7TS00a" crossorigin="anonymous"></script>
 
       <script src="{{ asset('js/scripts.js') }}"></script>
